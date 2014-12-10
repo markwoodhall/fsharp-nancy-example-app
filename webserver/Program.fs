a5e87fac6043250b337facd3aacb680608395398
Stephen Daves <stephen@stephendaves.com>
2014-12-10 00:17:11 -0500

    update banner

--- webserver/Program.fs
+++ webserver/Program.fs
@@ -7,10 +7,10 @@ type DemoApp () =
     inherit Nancy.NancyModule()
     do
         let Get = base.Get
-        Get.["/"] <- fun parameters -> "Hello from F#/Nancy on Heroku!" :> obj
+        Get.["/"] <- fun parameters -> "Hello from F#/Nancy on Dokku-Alt!" :> obj
 
 [<EntryPoint>]
-let main args = 
+let main args =
     let env_port = Environment.GetEnvironmentVariable("PORT")
     let port = if env_port = null then "5000" else env_port
     let uri = "0.0.0.0" + port
@@ -18,4 +18,4 @@ let main args =
     nancy_host.Start()
     printfn "listening on %s" uri
     while true do Console.ReadLine() |> ignore
-    0    
+    0
