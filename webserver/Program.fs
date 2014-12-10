619ee7d840cb12442a0cd7da29b97efc85f30c31
Stephen Daves <stephen@stephendaves.com>
2014-12-10 02:21:56 -0500

    use local

--- webserver/Program.fs
+++ webserver/Program.fs
@@ -13,9 +13,10 @@ type DemoApp () =
 let main args =
     let env_port = Environment.GetEnvironmentVariable("PORT")
     let port = if env_port = null then "5000" else env_port
-    let uri = "http://0.0.0.0:" + port
-    let nancy_host = new Nancy.Hosting.Self.NancyHost(new Uri(uri))
+    let localUri = "http://localhost:" + port
+    let productionUri = "http://mono1.do.daves.rocks:80"
+    let nancy_host = new Nancy.Hosting.Self.NancyHost(Uri(localUri))
     nancy_host.Start()
-    printfn "listening on %s" uri
+    printfn "listening on %s" localUri
     while true do Console.ReadLine() |> ignore
     0
