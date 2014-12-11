386e58b7ba195b00b10af71368ede13c90f91cd4
Stephen Daves <stephen@stephendaves.com>
2014-12-11 16:29:16 -0500

    * Program.fs: update main app
    * webserver.fsproj: project update

--- webserver/Program.fs
+++ webserver/Program.fs
@@ -1,21 +1,22 @@
-﻿module viewfiles.Main
+﻿module webserver.main
 
 open System
 open Nancy.Hosting
 
-type DemoApp () =
-    inherit Nancy.NancyModule()
-    do
-        let Get = base.Get
-        Get.["/"] <- fun parameters -> "Hello from F#/Nancy on Dokku-Alt!" :> obj
+let getUrl port = 
+    let env_port = Environment.GetEnvironmentVariable("PORT")
+    let port = if env_port = null then "5000" else env_port
+    "http://localhost:" + port
+
+let createLocalHost url =
+    let uri = Uri url
+    new Nancy.Hosting.Self.NancyHost(uri)
 
 [<EntryPoint>]
 let main args =
-    let env_port = Environment.GetEnvironmentVariable("PORT")
-    let port = if env_port = null then "5000" else env_port
-    let localUri = "http://localhost:" + port
-    let nancy_host = new Nancy.Hosting.Self.NancyHost(Uri(localUri))
-    nancy_host.Start()
-    printfn "listening on %s" localUri
+    let url = getUrl()
+    let host = url |> createLocalHost 
+    host.Start()
+    printfn "Listening on %s" url
     while true do Console.ReadLine() |> ignore
     0
